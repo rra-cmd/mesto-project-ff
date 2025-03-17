b00a7c962e003914bd5a5fbedca0ea2279f29249
Ruslan Aliev <ruslan.r.aliev@yandex.ru>
2025-03-17 18:55:40 +0300

    Версия 1. Первая попытка пройти ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardProperties, deleteCardCallback) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = cardProperties.name;
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = cardProperties.link;
+  cardImage.alt = cardProperties.name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCardCallback(cardElement);
+  });
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardProperties) => {
+  const newCard = createCard(cardProperties, deleteCard);
+  placesList.append(newCard);
+});
